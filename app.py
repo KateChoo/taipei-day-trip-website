9d1554c85e32dd6a5d3b4ebf1f17cb8feca11996
KateChoo <twodoor1993@gmail.com>
2021-04-23 18:19:20 +0800

    auth_plugin

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import re
 mydb = mysql.connector.connect(host='localhost',
                                user='k',
                                password='kpython',
-                               database="test"
+                               database="test",
+                               auth_plugin='mysql_native_password'
                                )
 
 app = Flask(__name__)
