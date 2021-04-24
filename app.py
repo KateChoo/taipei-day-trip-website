2268b07aa92348686a9b6edd260648937923dc1e
KateChoo <twodoor1993@gmail.com>
2021-04-24 13:43:12 +0800

    [fixed page 0]/api/attractions

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import *
 import mysql.connector
 import requests
-import math
 import re
 
 mydb = mysql.connector.connect(host='localhost',
@@ -31,6 +30,16 @@ def attraction(id):
     return render_template("attraction.html")
 
 
+@ app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+
+@ app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
+
 @ app.route('/api/attraction/<attractionId>')
 def attractionid(attractionId):  # /api/attraction/attractionid
     try:
@@ -50,14 +59,46 @@ def attractionid(attractionId):  # /api/attraction/attractionid
         return 'not yet'
 
 
-@ app.route("/booking")
-def booking():
-    return render_template("booking.html")
+@app.route('/api/attractions')  # /api/attractions?page=p
+def page():
+    try:
+        # page等號左邊 = page_num使用者輸入 #一定是字串
+        page_num = request.args.get("page", 0)
+        page_num = int(page_num)
+        print(page_num)
+        print(type(page_num))
 
+        #search = request.args.get("keyword")
 
-@ app.route("/thankyou")
-def thankyou():
-    return render_template("thankyou.html")
+        cursor.execute('SELECT id, name, category, description, address, transport, MRT, latitude, longitude, image  FROM t5 LIMIT 12'
+                       )
+        results = cursor.fetchall()
+        result = []
+        for r in results:
+            data = {
+                "id": r[0],
+                "name": r[1],
+                "category": r[2],
+                "description": r[3],
+                "address": r[4],
+                "transport": '公車：' + r[5],
+                "mrt": r[6],
+                "latitude": r[7],
+                "longitude": r[8],
+                "image": r[9]
+            }
+            # print(data)
+            result.append(data)
+            p_data = (
+                {
+                    "nextPage": page_num,
+                    "data": result
+                }
+            )
+        return jsonify(p_data)
+
+    except:
+        return 'not yet'
 
 
 app.run(host="0.0.0.0", debug=True, port=3000)
